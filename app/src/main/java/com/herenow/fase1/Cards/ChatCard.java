d74ab3476876a0ebaabc97abb0320295a7fd898f
sorrento <milenko@halat.cl>
2015-11-02 19:30:55 +0100

    creating Chat card

---
+++ app/src/main/java/com/herenow/fase1/Cards/ChatCard.java
@@ -0,0 +1,25 @@
+package com.herenow.fase1.Cards;
+
+import android.content.Context;
+
+import it.gmariotti.cardslib.library.internal.Card;
+
+/**
+ * Created by Milenko on 02/11/2015.
+ */
+public class ChatCard extends Card {
+
+    public ChatCard(Context context) {
+        super(context);
+    }
+
+    public ChatCard(Context context, int innerLayout) {
+        super(context, innerLayout);
+    }
+
+
+    @Override
+    protected void setupInnerLayout() {
+        super.setupInnerLayout();
+    }
+}
